c78fdf1b2af629cc666df4dd826e681c23e88664
learnersubha <learnersubha0@gmail.com>
2025-06-15 08:30:47 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@
   docker pull learnersubha/tws-online-shop-app-demo:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 learnersubha/tws-online-shop-app-demo:latest
